a80c9dfe507faea234648165100b59adf6b24bd2
Muhamedi <muhamed.r.krasniqi@gmail.com>
2023-04-22 19:50:01 +0200

    Update Palindrom_Number.py

--- Palindrom_Number.py
+++ Palindrom_Number.py
@@ -1,3 +1,10 @@
+# Language - Python
+# Date solved - 22/04/2023
+
+# - Solution 1
+# - Reverse the string and compare with the original
+# - Time Complexity O(n)
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         num = str(x)
